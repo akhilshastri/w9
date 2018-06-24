96e9a0ce9c379dae77965034558aefcf4e551115
akhilshastri <shastri.akhil@outlook.com>
2018-06-24 23:53:10 +0530

    AngularJS routing module added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {DashboardComponent} from "./routes/dashboard/dashboard.component";
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {DashboardComponent} from './routes/dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path:'dashboard',component:DashboardComponent},
-  {path:'**', redirectTo:'dashboard',pathMatch:'full'}
+  {path: 'dashboard', component: DashboardComponent},
+  {path: '**', redirectTo: 'dashboard', pathMatch: 'full'}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{useHash:true})],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
---
+++ src/assets/angular-js/1.4.9/ng-app.js
@@ -0,0 +1,26 @@
+
+// debugger;
+var app = angular.module('my-app',['ngRoute']);
+
+app.config(['$routeProvider','$locationProvider',function ($rp,$lp) {
+  debugger;
+  $lp.html5Mode(false);
+  $rp
+    .when('/view1',{
+    template:'<div> {{$name}} <a href="/assets/#view2"> View2 </a></div>',
+      controller:['$scope',function($scope){
+          $scope.name='View 1';
+      }]
+  }).when('/view2',{
+    template:'<div> {{$name}} <a href="/assets/#view1"> View1 </a> </div>',
+      controller:['$scope',function($scope){
+          $scope.name='View 2';
+      }]
+  }).otherwise({redirectTo: '/view1'});
+}]) ;
+
+app.run(function(){
+  // debugger;
+});
+
+// alert('app-loaded');
